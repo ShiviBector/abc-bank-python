bbcb15333705d393c5e6bdfdb2cc1eecb3733780
ShiviBector <bector.shivi@gmail.com>
2016-05-10 15:44:24 -0400

    Update customer_tests.py

--- tests/customer_tests.py
+++ tests/customer_tests.py
@@ -1,21 +1,25 @@
 from nose.tools import assert_equals, nottest
 
-from account import Account, CHECKING, SAVINGS
+from account import Account, CHECKING, SAVINGS, MAXI_SAVINGS
 from customer import Customer
 
 
 def test_statement():
     checkingAccount = Account(CHECKING)
     savingsAccount = Account(SAVINGS)
+    maxSavingAccount=Account(MAXI_SAVINGS)
     henry = Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount)
     checkingAccount.deposit(100.0)
     savingsAccount.deposit(4000.0)
     savingsAccount.withdraw(200.0)
+    maxSavingAccount.deposit(500.0)
+    maxSavingAccount.withdraw(200.0)
     assert_equals(henry.getStatement(),
                   "Statement for Henry" +
                   "\n\nChecking Account\n  deposit $100.00\nTotal $100.00" +
                   "\n\nSavings Account\n  deposit $4000.00\n  withdrawal $200.00\nTotal $3800.00" +
-                  "\n\nTotal In All Accounts $3900.00")
+                  "\n\nMAxi Savings Account\n  deposit $ 500.00\n withdrawal $200.00\nTotal $300.00" +
+                  "\n\nTotal In All Accounts $4200.00")
 
 
 def test_oneAccount():
@@ -28,9 +32,15 @@ def test_twoAccounts():
     oscar.openAccount(Account(CHECKING))
     assert_equals(oscar.numAccs(), 2)
 
-
-@nottest
 def test_threeAccounts():
     oscar = Customer("Oscar").openAccount(Account(SAVINGS))
     oscar.openAccount(Account(CHECKING))
+    oscar.openAccount(Account(MAXI_SAVINGS)
     assert_equals(oscar.numAccs(), 3)
+    
+@nottest
+def test_FourAccounts():
+    oscar = Customer("Oscar").openAccount(Account(SAVINGS))
+    oscar.openAccount(Account(CHECKING))
+    oscar.openAccount(Account(MAXI_SAVINGS)
+    assert_equals(oscar.numAccs(), 4)
